992d83ba7f980a5ec7eb1838a1bbd0f15517bd32
rogeriop <pinheiroxrogerio@gmail.com>
2015-05-12 07:09:56 -0300

    fim 01 Testes manuais ou automatizados?

---
+++ src/TesteAutomatizado.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TesteAutomatizado {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:/Users/rogeriop/ALURA/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+//		firefoxDriver.get("http://www.google.com.br");
+		driver.get("http://www.bing.com");
+		WebElement findElement = driver.findElement(By.name("q"));
+		findElement.sendKeys("Caelum");
+		findElement.submit();
+	}
+}
